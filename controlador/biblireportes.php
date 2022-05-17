c2d9fd257ac0a0fd433225b608f52a2f06e27d57
GhibaudoJuan <ghibaudjuan@gmail.com>
2022-05-17 19:24:54 -0300

    c

--- controlador/biblireportes.php
+++ controlador/biblireportes.php
@@ -48,21 +48,22 @@ padding-bottom:1rem;
 <div class="span-2 ajuste"><span>Intervalo de tiempo</span></div>
 <div class="ajuste" >
 <select id="tiempo" name="tiempo" class="index" style="width:12rem;">
-  <option value=" >= current_date - interval '24 hours' ">&Uacuteltimas 24 horas</option>
-  <option value=" >= current_date - interval '1 month' ">&Uacuteltimo mes</option>
-  <option value=" >= current_date - interval '3 month' ">&Uacuteltimos 3 meses</option>
   <option value="">Todos</option>
+  <option value=" >= current_date - interval '3 month' ">&Uacuteltimos 3 meses</option>
+  <option value=" >= current_date - interval '1 month' ">&Uacuteltimo mes</option>
+  <option value=" >= current_date - interval '24 hours' ">&Uacuteltimas 24 horas</option>
+  
 </select>
 </div>
 </div>
 
 <div class="flex">
-<div class="span-2 ajuste"><span>Agrupar segun:</span></div>
+<div class="span-2 ajuste"><span>Agrupar seg&uacuten:</span></div>
 <div class="ajuste" >
 <select id="cantidad" name="cantidad" class="index" style="width:12rem;">
   <option value="1">Todos</option>
-  <option value="2">Segun usuario</option>
-  <option value="3">Segun material</option>
+  <option value="2">Seg&uacuten usuario</option>
+  <option value="3">Seg&uacuten material</option>
 </select>
 </div>
 </div>
@@ -70,37 +71,6 @@ padding-bottom:1rem;
 <input type="hidden"  id="sel1" name="select" value=" count(*) as cant ">
 
 
-<?php /*
-
-<div class="flex espacio">
-<div class="ajuste" >
-<label class="span-2"  for="sel1">Cantidad
-</label></div>
-<div class="ajuste">
-<input type="radio"  id="sel1" name="select" value=" count(*) as cant " checked>
-</div>
-</div>
-
-<div class="flex espacio">
-<div class="ajuste" >
-<label class="span-2"  for="sel2">Atributos
-</label></div>
-<div class="ajuste">
-<input type="radio"  id="sel2" name="select" value="atri" >
-</div>
-</div>
-
-
-
-<div class="flex" id="atri">
-<div class="ajuste "></div>
-<div class="ajuste ">
-<input type="checkbox" id="user" name="usuario" value=" nombre" >
-<label class="span-2" for="user">Usuario</label>
-</div>
-</div>
-
-*/?>
 
 
 <div class="alignr"><button type="submit" class="indexbutton">Registrar</button></div>
--- modelo/reportes.php
+++ modelo/reportes.php
@@ -15,7 +15,7 @@
 
 
    
-    $html2pdf = new Html2Pdf('P','A4','es','true','UTF-8');
+    $html2pdf = new Html2Pdf('P','A4','es','true','UTF-8',array(20,10,10,10));
    
     $html2pdf->writeHTML($html);
     
