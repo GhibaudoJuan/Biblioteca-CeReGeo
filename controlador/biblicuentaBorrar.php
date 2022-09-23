5642547612680bfde82c7fb1302219b26368b41f
GhibaudoJuan <70353467+GhibaudoJuan@users.noreply.github.com>
2022-09-23 15:10:39 -0300

    Update biblicuentaBorrar.php

--- controlador/biblicuentaBorrar.php
+++ controlador/biblicuentaBorrar.php
@@ -16,7 +16,7 @@ if(!isset($_SESSION))session_start(); ?>
 <div class="modal-body">
 <input type='hidden' id="borrar1" name ='id' >
 
-<label for="borrar1" style="font-size: 25px;">&#191Esta seguro de que quiere eliminarlo? </label>
+<label for="borrar1" style="font-size: 25px;">&#191Est&aacute seguro de que quiere eliminarlo? </label>
 <br><br>
 <div class="input-group m-div">
 <label class="input-group-text">Id</label>
