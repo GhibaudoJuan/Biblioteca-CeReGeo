e05b8ff7be7a9220936cc069b679df03f2366a23
GhibaudoJuan <70353467+GhibaudoJuan@users.noreply.github.com>
2022-09-23 15:09:19 -0300

    Update borrarTodo.php

--- controlador/borrarTodo.php
+++ controlador/borrarTodo.php
@@ -13,7 +13,7 @@
 
 <form action = "../modelo/borrarTodo.php" method="post" >
 <div class="modal-body">
-<span style="font-size: 25px;">&#191Esta seguro de que quiere eliminar todo? </span>
+<span style="font-size: 25px;">&#191Est&aacute seguro de que quiere eliminar todo? </span>
 
 <br>
 <input type='hidden' name="delete" id="delete" value="<?php echo $delete;?>">
