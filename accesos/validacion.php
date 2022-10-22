5d285109af6e09a19e6f0773b77af745a30459ad
GhibaudoJuan <ghibaudjuan@gmail.com>
2022-10-22 15:54:11 -0300

    Update validacion.php

--- accesos/validacion.php
+++ accesos/validacion.php
@@ -4,16 +4,19 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 if(!isset($_SESSION['user'])){
-header('location:../index.php');
+//header('location:../index.php');
+echo '<script>wondow.location="location:../index.php"</script>';
 }
 if((isset($_SESSION['val']))&&($_SESSION['val']==false)){
-header('location:../index.php');
+    //header('location:../index.php');
+    echo '<script>wondow.location="location:../index.php"</script>';
 }
 
 function validaracceso($tipo){
     
     if($_SESSION['tipouser']>$tipo){
-        header('location:../index.php');
+        //header('location:../index.php');
+        echo '<script>wondow.location="location:../index.php"</script>';
     }
     
 }
