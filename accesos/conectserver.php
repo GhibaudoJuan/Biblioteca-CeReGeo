ce9ac623a0dddff9d728e043bfc20167a906f2e0
GhibaudoJuan <70353467+GhibaudoJuan@users.noreply.github.com>
2022-10-31 16:33:50 -0300

    Update conectserver.php

--- accesos/conectserver.php
+++ accesos/conectserver.php
@@ -4,5 +4,5 @@ $server="127.0.0.1"; //direccion del server
 $basedatos="biblioteca"; //nombre de la base de datos
 $usuario="juan"; //usuario para la base de datos
 $contra="juan";  //contraseña para la base de datos
-$conect="host=".$server." dbname=".$basedatos." user=".$usuario." password=".$contra; //coneccion a la base de datos
+$conect="host=".$server." dbname=".$basedatos." user=".$usuario." password=".$contra; //conexión a la base de datos
 ?>
