0ad4478938b316c3a0bf64cd2ac63ef65d20d8d8
GhibaudoJuan <70353467+GhibaudoJuan@users.noreply.github.com>
2023-03-13 14:15:59 -0300

    Update validacion.php

--- accesos/validacion.php
+++ accesos/validacion.php
@@ -4,19 +4,19 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 if(!isset($_SESSION['user'])){
-//header('location:../index.php');
-echo '<script>window.location="location:../index.php"</script>';
+header('location:../index.php');
+//echo '<script>window.location="location:../index.php"</script>';
 }
 if((isset($_SESSION['val']))&&($_SESSION['val']==false)){
-    //header('location:../index.php');
-    echo '<script>window.location="location:../index.php"</script>';
+    header('location:../index.php');
+    //echo '<script>window.location="location:../index.php"</script>';
 }
 
 function validaracceso($tipo){
     
     if($_SESSION['tipouser']>$tipo){
-        //header('location:../index.php');
-        echo '<script>window.location="location:../index.php"</script>';
+        header('location:../index.php');
+        //echo '<script>window.location="location:../index.php"</script>';
     }
     
 }
